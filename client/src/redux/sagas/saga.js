f1887f66e2a9f90f082e7cf8700905d6644242eb
Мария Лось <marialos@MacBook-Air-Maria.local>
2022-02-04 14:04:37 +0300

    create-redux

---
+++ client/src/redux/sagas/saga.js
@@ -0,0 +1,38 @@
+import { call, put, takeEvery } from 'redux-saga/effects'
+import { initItemsAC, deleteItemsAC, updateItemsAC, addItemsAC } from '../actionCreators/itemsAC'
+
+async function fetchData({ url, method, headers, body }) {
+  const response = await fetch(url, { method, headers, body });
+  return (await response.json());
+}
+
+function* getItemsAsync() {
+  const items = yield call(fetchData, { url: process.env.REACT_APP_ITEMS_URL });
+
+  yield put(initItemsAC(items));
+}
+
+function* deleteItemAsync(action) {
+  const id = yield call(fetchData, { url: `${process.env.REACT_APP_ITEMS_URL}/${action.payload}`, headers: { 'Content-Type': 'Application/json' }, method: 'DELETE' });
+
+  yield put(deleteItemsAC(id));
+}
+
+function* putItemAsync(action) {
+  const updatedItem = yield call(fetchData, { url: `${process.env.REACT_APP_ITEMS_URL}/${action.payload.id}`, headers: { 'Content-Type': 'Application/json' }, method: 'PUT', body: JSON.stringify(action.payload) });
+
+  yield put(updateItemsAC(updatedItem));
+}
+
+function* postItemAsync(action) {
+  const newItem = yield call(fetchData, { url: `${process.env.REACT_APP_ITEMS_URL}`, headers: { 'Content-Type': 'Application/json' }, method: 'POST', body: JSON.stringify(action.payload) });
+
+  yield put(addItemsAC(newItem));
+}
+
+export function* globalWatcher() {
+  yield takeEvery("FETCH_GET_ITEMS", getItemsAsync);
+  yield takeEvery("FETCH_DELETE_ITEM", deleteItemAsync);
+  yield takeEvery("FETCH_PUT_ITEM", putItemAsync);
+  yield takeEvery("FETCH_POST_ITEM", postItemAsync);
+}
---
+++ server/routes/item.route.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const { Item } = require('../db/models');
+
+router.route('/')
+  .get((req, res) => {
+    Item.findAll()
+      .then((allItems) => res.json(allItems))
+      .catch((error) => console.log(error));
+  })
+
+
+router.post('/', async (req, res) => {
+
+  const fileName = `${uuid.v4()}.jpeg`;
+  img.mv(path.resolve(__dirname, "../..", "public/images", fileName));
+  const photoPath = `/images/${fileName}`;
+
+  await Pet.create({
+    name,
+    description,
+    gender,
+    age,
+    photo: photoPath,
+    species,
+  });
+    Item.create(req.body)
+      .then((newItem) => res.status(201).json(newAnimal))
+      .catch((error) => res.status(500).json(error));
+  });
+
+router.route('/:id')
+  .put((req, res) => {
+    const { id } = req.params;
+
+    Animal.update(req.body, { where: { id }, returning: true })
+      .then((updatedAnimal) => res.json(updatedAnimal))
+      .catch((error) => res.status(500).json(error));
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    Animal.destroy({ where: { id } })
+      .then((data) => (data ? res.json(id) : res.status(404).json(data)))
+      .catch((error) => res.status(500).json(error));
+  });
+
+module.exports = router;
