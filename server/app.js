4fc6ae740dba6a6952ae7359251114d6b30dfd05
Mansur Makhmutov <sir.makhmutov@gmail.com>
2022-02-04 16:49:51 +0300

    reg conflict resolution

--- server/app.js
+++ server/app.js
@@ -1,45 +1,18 @@
-// const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-// const cookieParser = require("cookie-parser");
-// const expressSession = require("express-session");
-// const FileStore = require("session-file-store")(expressSession);
 const cors = require('cors');
 
 const PORT = 4000;
 
 const app = express();
 
-// app.use(express.static(path.join(__dirname, "public")));
-
 const registrationRouter = require('./routes/registration.router');
-
-// const sessionConfig = {
-//   store: new FileStore(),
-//   name: "user_sid",
-//   secret: process.env.SESSION_SECRET ?? "test",
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: {
-//     maxAge: 1000 * 60 * 60 * 12,
-//     httpOnly: true,
-//   },
-// };
 
 app.use(cors());
 app.use(morgan('dev'));
-// app.use(cookieParser());
-// app.use(expressSession(sessionConfig));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api/reg', registrationRouter);
 
-// app.use('/api/reg', (req, res) => {
-//   const { body } = req;
-//   console.log(body);
-// });
-
-app.listen(PORT, () => {
-  console.info('The server is up and running on', PORT);
-});
+app.listen(PORT);
