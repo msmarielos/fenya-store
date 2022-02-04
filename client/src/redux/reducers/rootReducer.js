35c4177c41de0e4dfd6c16e5c230f86185f7a16c
Мария Лось <marialos@Air-Maria.Dlink>
2022-02-04 22:57:26 +0300

    created list reducer and fixed item

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { itemReducer } from '../reducers/itemReducer';
 import { categoriesReducer } from './categoriesReducer';
+import { listsReducer } from './listsReducer';
 
 export const rootReducer = combineReducers({
   items: itemReducer,
   categories: categoriesReducer,
+  lists: listsReducer,
 });
