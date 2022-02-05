cdf982cffdc2bfc66bd0ffb8aba3eebb25392437
Andrey Barkhatov <barkhatovandrew@gmail.com>
2022-02-05 15:27:00 +0300

    nav bar

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import Navigation from '../Navigation/Navigation';
 import SearchPanel from '../SearchPanel/SearchPanel';
 import TopPanel from '../TopPanel/TopPanel';
 import './normalize.css';
@@ -9,7 +8,6 @@ function App() {
     <div className="root-container">
       <TopPanel />
       <SearchPanel />
-      <Navigation />
       <Outlet />
     </div>
   );
--- client/src/components/SearchPanel/SearchPanel.jsx
+++ client/src/components/SearchPanel/SearchPanel.jsx
@@ -1,5 +1,6 @@
 import './SearchPanel.scss';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
+import Navigation from '../Navigation/Navigation';
 
 export default function SearchPanel() {
   return (
@@ -7,7 +8,9 @@ export default function SearchPanel() {
       <div className="search-panel">
         <div className="search-container">
           <div className="logo">
-            <Link to={'/'}><h1>Fenya Store</h1></Link>
+            <Link to={'/'}>
+              <h1>Fenya Store</h1>
+            </Link>
             <p>Интернет зоомагазин</p>
           </div>
           <form>
@@ -15,6 +18,7 @@ export default function SearchPanel() {
             <button className="regular-btn">Корзина</button>
           </form>
         </div>
+        <Navigation />
       </div>
     </>
   );
