120585dfb4a205fcf9dc5bbbe263c2e4251bbf75
BarkhatovAndrew <BarkhatovAndrew@users.noreply.github.com>
2022-02-06 11:12:33 +0000

    Apply formatting changes

--- server/src/routes/item.route.js
+++ server/src/routes/item.route.js
@@ -6,63 +6,79 @@ const { storage } = require('../storage');
 const upload = multer({ storage });
 
 router.get('/food/all', (req, res) => {
-  Item.findAll({ where: { categoryType_id: [1, 4] } }).then((items) => res.json(items));
+  Item.findAll({ where: { categoryType_id: [1, 4] } }).then(items =>
+    res.json(items)
+  );
 });
 
 router.get('/cats/food', (req, res) => {
-  Item.findAll({ where: { categoryType_id: 1 } }).then((items) => res.json(items));
+  Item.findAll({ where: { categoryType_id: 1 } }).then(items =>
+    res.json(items)
+  );
 });
 
 router.get('/dogs/food', (req, res) => {
-  Item.findAll({ where: { categoryType_id: 4 } }).then((items) => res.json(items));
+  Item.findAll({ where: { categoryType_id: 4 } }).then(items =>
+    res.json(items)
+  );
 });
 
 router.get('/dogs/toys', (req, res) => {
-  Item.findAll({ where: { categoryType_id: 5 } }).then((items) => res.json(items));
+  Item.findAll({ where: { categoryType_id: 5 } }).then(items =>
+    res.json(items)
+  );
 });
 
 router.get('/cats/toys', (req, res) => {
-  Item.findAll({ where: { categoryType_id: 2 } }).then((items) => res.json(items));
+  Item.findAll({ where: { categoryType_id: 2 } }).then(items =>
+    res.json(items)
+  );
 });
 
 router.get('/toys/all', (req, res) => {
-  Item.findAll({ where: { categoryType_id: [5, 2] } }).then((items) => res.json(items));
+  Item.findAll({ where: { categoryType_id: [5, 2] } }).then(items =>
+    res.json(items)
+  );
 });
 
 router.get('/dogs/clothes', (req, res) => {
-  Item.findAll({ where: { categoryType_id: 3 } }).then((items) => res.json(items));
+  Item.findAll({ where: { categoryType_id: 3 } }).then(items =>
+    res.json(items)
+  );
 });
 
 router.get('/cats/clothes', (req, res) => {
-  Item.findAll({ where: { categoryType_id: 6 } }).then((items) => res.json(items));
+  Item.findAll({ where: { categoryType_id: 6 } }).then(items =>
+    res.json(items)
+  );
 });
 
 router.get('/clothes/all', (req, res) => {
-  Item.findAll({ where: { categoryType_id: [3, 6] } }).then((items) => res.json(items));
+  Item.findAll({ where: { categoryType_id: [3, 6] } }).then(items =>
+    res.json(items)
+  );
 });
 
 router.get('/:itemId', (req, res) => {
   const id = req.params.itemId;
-  Item.findOne({ where: { id } }).then((item) => res.json(item));
+  Item.findOne({ where: { id } }).then(item => res.json(item));
 });
 
 router.get('/', async (req, res) => {
   Item.findAll()
-    .then((allItems) => res.json(allItems))
-    .catch((error) => console.log(error));
+    .then(allItems => res.json(allItems))
+    .catch(error => console.log(error));
 });
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
   Item.destroy({ where: { id } })
-    .then((data) => (data ? res.json(id) : res.status(404).json(data)))
-    .catch((error) => res.status(500).json(error));
+    .then(data => (data ? res.json(id) : res.status(404).json(data)))
+    .catch(error => res.status(500).json(error));
 });
 
 router.post('/', upload.single('img'), async (req, res) => {
-  const {
-    title, description, price, type, category, amount,
-  } = req.body;
+  const { title, description, price, type, category, amount } = req.body;
   const { filename } = req.file;
 
   const categoryType_id = await CategoryType.findOne({
@@ -90,18 +106,19 @@ router.post('/', upload.single('img'), async (req, res) => {
 
 router.put('/:id', (req, res) => {
   const itemId = req.params.id;
-  const {
-    title, description, price, amount,
-  } = req.body;
+  const { title, description, price, amount } = req.body;
 
   Item.update(
     {
-      title, description, price, amount,
+      title,
+      description,
+      price,
+      amount,
     },
-    { where: { itemId }, returning: true },
+    { where: { itemId }, returning: true }
   )
-    .then((updatedItem) => res.json(updatedItem))
-    .catch((error) => res.status(500).json(error));
+    .then(updatedItem => res.json(updatedItem))
+    .catch(error => res.status(500).json(error));
 });
 
 module.exports = router;
