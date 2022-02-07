504608475bf62d4fc5b72ccbc182a53a3042ae11
Andrey Barkhatov <barkhatovandrew@gmail.com>
2022-02-07 10:32:39 +0300

    add ratingstars

--- client/src/components/ReviewCard/ReviewCard.jsx
+++ client/src/components/ReviewCard/ReviewCard.jsx
@@ -1,8 +1,15 @@
+import ReactStars from 'react-rating-stars-component';
+
 export default function ReviewCard({ item }) {
   return (
     <>
       <h5>{item.title}</h5>
-      <p>Оценка: {item.rating}</p>
+      <ReactStars
+        value={item.rating}
+        size={24}
+        edit={false}
+        activeColor="#ffd700"
+      />
       <p>{item.description}</p>
       <hr />
     </>
