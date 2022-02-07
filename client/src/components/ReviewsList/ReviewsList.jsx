734cbcd4c82c25309bb7695e983a78a4aeffd505
Andrey Barkhatov <barkhatovandrew@gmail.com>
2022-02-07 11:16:57 +0300

    style fixes

--- client/src/components/ReviewsList/ReviewsList.jsx
+++ client/src/components/ReviewsList/ReviewsList.jsx
@@ -1,18 +1,18 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import ReviewCard from '../ReviewCard/ReviewCard';
 import ReviewForm from '../ReviewForm/ReviewForm';
 
 export default function ReviewsList() {
   const dispatch = useDispatch();
-  const pathArr = window.location.pathname.split('/');
-  const id = pathArr[pathArr.length - 1];
+  const params = useParams();
 
   const { reviews } = useSelector(state => state.reviews);
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_GET_REVIEWS', payload: id });
-  }, [dispatch, id]);
+    dispatch({ type: 'FETCH_GET_REVIEWS', payload: params.id });
+  }, [dispatch, params.id]);
 
   return (
     <>
