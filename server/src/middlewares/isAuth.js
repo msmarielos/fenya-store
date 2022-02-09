999e5f80707f23139e6539b73fc78fa0ed025ef6
Mansur Makhmutov <sir.makhmutov@gmail.com>
2022-02-09 03:26:24 +0300

    take admin drom roken

--- server/src/middlewares/isAuth.js
+++ server/src/middlewares/isAuth.js
@@ -4,9 +4,13 @@ async function isAuth(req, res, next) {
   try {
     const authHeader = req.get('Authorization'); // получаю из header токен, проверяю правильный ли токен
     const token = authHeader.split(' ')[1];
-    const { userId, email } = jwt.verify(token, process.env.ACCESS_TOKEN);
+    const { userId, email, isAdmin } = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN
+    );
     req.userId = userId;
     req.email = email;
+    req.isAdmin = isAdmin;
 
     next();
   } catch (e) {
