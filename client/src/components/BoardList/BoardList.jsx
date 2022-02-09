7401122f57a999c05bc6c74242d4bc18438de134
alexrad555 <arayduyk@gmail.com>
2022-02-09 22:42:34 +0300

    fix_done

--- client/src/components/BoardList/BoardList.jsx
+++ client/src/components/BoardList/BoardList.jsx
@@ -54,7 +54,9 @@ export default function BoardList() {
           unpublishedAnimals.map(animal => (
             <li key={animal.id}>
               {animal.name}
-              <img src={`storage/${animal.img}`} />
+              <img src={animal.img.split('/')[0] === 'https:'
+                ? animal.img
+                : `/storage/${animal.img}`} />
               <button data-id={animal.id} onClick={putFetch}>
                 Опубликовать
               </button>
@@ -70,7 +72,9 @@ export default function BoardList() {
           publishedAnimals.map(animal => (
             <li key={animal.id}>
               {animal.name}
-              <img src={`storage/${animal.img}`} />
+              <img src={animal.img.split('/')[0] === 'https:'
+                ? animal.img
+                : `/storage/${animal.img}`} />
               <button data-id={animal.id} onClick={deleteFetch}>
                 Удалить
               </button>
