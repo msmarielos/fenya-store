a018f7c3570d699268b691b66643e47bbb722096
Andrey Barkhatov <barkhatovandrew@gmail.com>
2022-02-10 18:26:19 +0300

    ushel domoi

--- server/src/app.js
+++ server/src/app.js
@@ -40,9 +40,9 @@ app.use('/reviews', reviewRouter);
 app.use('/animals', animalRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve('../../client/build')));
+  app.use(express.static(path.resolve('../client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve('../../client/build/index.html'));
+    res.sendFile(path.resolve('../client/build/index.html'));
   });
 }
 
