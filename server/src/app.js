40891be10954faab0e05133ece1012b21b342e23
Andrey Barkhatov <barkhatovandrew@gmail.com>
2022-02-10 15:11:10 +0300

    try 1

--- server/src/app.js
+++ server/src/app.js
@@ -30,6 +30,13 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  });
+}
+
 app.use('/items', itemRouter);
 app.use('/categories', categoryRouter);
 app.use('/lists', listsRouter);
