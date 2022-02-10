dd8fc2b58fbe402a6395c36459e31346e71f938d
Andrey Barkhatov <barkhatovandrew@gmail.com>
2022-02-10 18:17:40 +0300

    12331

--- server/src/app.js
+++ server/src/app.js
@@ -42,7 +42,7 @@ app.use('/animals', animalRouter);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+    res.sendFile(path.resolve('../client/build/index.html'));
   });
 }
 
