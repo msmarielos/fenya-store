5d6a553bbc58d6a5963cca3442aa42fa8c8e37f9
alexrad555 <arayduyk@gmail.com>
2022-02-10 23:17:54 +0300

    done_order_card

--- client/src/components/OrederUserCard/OrederUserCard.jsx
+++ client/src/components/OrederUserCard/OrederUserCard.jsx
@@ -7,7 +7,12 @@ function OrederUserCard({ item, number }) {
     <div>
       <p>Заказ номер: {number}</p>
       <ul>
-       {item.OrderItems.map((el) => <li><p>Товар: {el.Item.title}</p><p>Количество: {el.count}</p></li>)}
+       {item.OrderItems.map((el) => <li><img src={
+              el.Item.img.split('/')[0] === 'https:'
+                ? el.Item.img
+                : `storage/${el.Item.img}`
+            }
+            alt="картика"/><p>Товар: {el.Item.title}</p><p>Количество: {el.count}</p></li>)}
       </ul>
       <p>Общая сумма: {item.OrderItems.reduce((acc, num) => acc + num.Item.price * num.count, 0)}</p>
     </div>
