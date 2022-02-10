fc993ba1932561446f9152688b0948cf827684f8
alexrad555 <arayduyk@gmail.com>
2022-02-10 15:20:56 +0300

    rel1

--- client/src/components/App/Profile/Profile.jsx
+++ client/src/components/App/Profile/Profile.jsx
@@ -1,12 +1,8 @@
 function Profile(props) {
-  function logout() {
-    localStorage.removeItem('token');
-    window.location.reload();
-  }
+
 
   return (
     <div>
-      <button onClick={logout}>Выйти</button>
     </div>
   );
 }
