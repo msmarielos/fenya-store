c3460b3b94a8cf581b87badca4444812b045ba1d
alexrad555 <alexrad555@users.noreply.github.com>
2022-02-11 08:41:56 +0000

    Apply formatting changes

--- client/src/components/OrderList/OrderList.jsx
+++ client/src/components/OrderList/OrderList.jsx
@@ -14,7 +14,7 @@ export default function OrderList() {
     dispatch({ type: 'FETCH_DELETE_ORDER', payload: id });
   };
 
-  console.log(orders,'orders')
+  console.log(orders, 'orders');
   return (
     <ul>
       {orders?.length ? (
